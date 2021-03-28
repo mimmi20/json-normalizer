a57f4192b52b6d0608d8a09f6c00ea83cf9d467d
Thomas Müller <mimmi20@live.de>
2021-03-28 21:30:22 +0200

    update coding standard

--- tests/FormatNormalizerTest.php
+++ tests/FormatNormalizerTest.php
@@ -9,26 +9,47 @@
  */
 
 declare(strict_types = 1);
+
 namespace JsonTest\Normalizer;
 
+use Ergebnis\Json\Normalizer\Exception\InvalidIndentStringException;
+use Ergebnis\Json\Normalizer\Exception\InvalidJsonEncodedException;
+use Ergebnis\Json\Normalizer\Exception\InvalidJsonEncodeOptionsException;
+use Ergebnis\Json\Normalizer\Exception\InvalidNewLineStringException;
+use Ergebnis\Json\Normalizer\Exception\NormalizedInvalidAccordingToSchemaException;
+use Ergebnis\Json\Normalizer\Exception\OriginalInvalidAccordingToSchemaException;
+use Ergebnis\Json\Normalizer\Exception\SchemaUriCouldNotBeReadException;
+use Ergebnis\Json\Normalizer\Exception\SchemaUriCouldNotBeResolvedException;
+use Ergebnis\Json\Normalizer\Exception\SchemaUriReferencesDocumentWithInvalidMediaTypeException;
+use Ergebnis\Json\Normalizer\Exception\SchemaUriReferencesInvalidJsonDocumentException;
 use Ergebnis\Json\Normalizer\Format\Format;
 use Ergebnis\Json\Normalizer\Format\Indent;
 use Ergebnis\Json\Normalizer\Format\JsonEncodeOptions;
 use Ergebnis\Json\Normalizer\Format\NewLine;
 use Ergebnis\Json\Normalizer\Json;
+use ExceptionalJSON\EncodeErrorException;
 use Json\Normalizer\FormatNormalizer;
 use JsonClass\JsonInterface;
+use PHPUnit\Framework\Exception;
 use PHPUnit\Framework\TestCase;
+use ReflectionException;
+use ReflectionProperty;
+use SebastianBergmann\RecursionContext\InvalidArgumentException;
+use stdClass;
+use UnexpectedValueException;
+
+use function assert;
+
+use const JSON_HEX_QUOT;
+use const JSON_PRETTY_PRINT;
 
 final class FormatNormalizerTest extends TestCase
 {
     /**
-     * @throws \Ergebnis\Json\Normalizer\Exception\InvalidJsonEncodeOptionsException
-     * @throws \Ergebnis\Json\Normalizer\Exception\InvalidIndentStringException
-     * @throws \Ergebnis\Json\Normalizer\Exception\InvalidNewLineStringException
-     * @throws \UnexpectedValueException
-     *
-     * @return void
+     * @throws InvalidJsonEncodeOptionsException
+     * @throws InvalidIndentStringException
+     * @throws InvalidNewLineStringException
+     * @throws UnexpectedValueException
      */
     public function testNormalizeMissingPrettyPrint(): void
     {
@@ -39,36 +60,34 @@ final class FormatNormalizerTest extends TestCase
             false
         );
 
-        $this->expectException(\UnexpectedValueException::class);
+        $this->expectException(UnexpectedValueException::class);
         $this->expectExceptionMessage('This Normalizer requires the JSON_PRETTY_PRINT option to be set.');
 
-        /* @var Format $format */
+        assert($format instanceof Format);
         new FormatNormalizer($format);
     }
 
     /**
-     * @throws \Ergebnis\Json\Normalizer\Exception\InvalidIndentStringException
-     * @throws \Ergebnis\Json\Normalizer\Exception\InvalidNewLineStringException
-     * @throws \PHPUnit\Framework\Exception
-     * @throws \UnexpectedValueException
-     * @throws \ReflectionException
-     * @throws \Ergebnis\Json\Normalizer\Exception\InvalidJsonEncodeOptionsException
-     * @throws \Ergebnis\Json\Normalizer\Exception\InvalidJsonEncodedException
-     * @throws \Ergebnis\Json\Normalizer\Exception\NormalizedInvalidAccordingToSchemaException
-     * @throws \Ergebnis\Json\Normalizer\Exception\OriginalInvalidAccordingToSchemaException
-     * @throws \Ergebnis\Json\Normalizer\Exception\SchemaUriCouldNotBeReadException
-     * @throws \Ergebnis\Json\Normalizer\Exception\SchemaUriCouldNotBeResolvedException
-     * @throws \Ergebnis\Json\Normalizer\Exception\SchemaUriReferencesDocumentWithInvalidMediaTypeException
-     * @throws \Ergebnis\Json\Normalizer\Exception\SchemaUriReferencesInvalidJsonDocumentException
-     * @throws \ExceptionalJSON\EncodeErrorException
-     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
-     *
-     * @return void
+     * @throws InvalidIndentStringException
+     * @throws InvalidNewLineStringException
+     * @throws Exception
+     * @throws UnexpectedValueException
+     * @throws ReflectionException
+     * @throws InvalidJsonEncodeOptionsException
+     * @throws InvalidJsonEncodedException
+     * @throws NormalizedInvalidAccordingToSchemaException
+     * @throws OriginalInvalidAccordingToSchemaException
+     * @throws SchemaUriCouldNotBeReadException
+     * @throws SchemaUriCouldNotBeResolvedException
+     * @throws SchemaUriReferencesDocumentWithInvalidMediaTypeException
+     * @throws SchemaUriReferencesInvalidJsonDocumentException
+     * @throws EncodeErrorException
+     * @throws InvalidArgumentException
      */
     public function testNormalizeWithPrettyPrint(): void
     {
         $jsonOptions                = JSON_HEX_QUOT | JSON_PRETTY_PRINT;
-        $decodedJson                = new \stdClass();
+        $decodedJson                = new stdClass();
         $decodedJson->{'Test-Json'} = false;
         $encodedJson                = '{"Test-Json": false}';
         $encodedJson2               = "{\n    \"Test-Json\": false\n}";
@@ -82,7 +101,7 @@ final class FormatNormalizerTest extends TestCase
             true
         );
 
-        /** @var Format $format */
+        assert($format instanceof Format);
         $object = new FormatNormalizer($format);
 
         $jsonClass = $this->getMockBuilder(JsonInterface::class)
@@ -94,7 +113,7 @@ final class FormatNormalizerTest extends TestCase
             ->with($decodedJson, $jsonOptions)
             ->willReturn($encodedJson2);
 
-        $refProperty = new \ReflectionProperty($object, 'jsonClass');
+        $refProperty = new ReflectionProperty($object, 'jsonClass');
         $refProperty->setAccessible(true);
         $refProperty->setValue($object, $jsonClass);
 
